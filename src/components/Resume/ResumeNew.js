afb5be95311d213d4647043527d5270c34ecde20
David <99383802+Daviddaill@users.noreply.github.com>
2023-09-08 10:29:58 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Daviddaill/my-resume/master/david-daillere.pdf";
+  "https://github.com/Daviddaill/resume/blob/main/Resume - David Daillere | Front-End Developer.pdf?raw=true";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
